efe28cdfe33fddf653e71232782a941922c332d1
Matthew Burgess <boog397@gmail.com>
2024-02-04 21:18:24 -0600

    Organize existing code into individual methods

--- javascript.js
+++ javascript.js
@@ -1,13 +1,22 @@
-const container = document.querySelector(".container");
-const grid = document.querySelector(".grid");
+setContainerSize();
+drawGrid();
 
-const gridWidth = 16;
-let gridItem;
-for (let i = 0; i < gridWidth * gridWidth; i++) {
-    gridItem = document.createElement("div");
-    gridItem.classList.toggle("grid-item");
-    grid.appendChild(gridItem);
+
+function setContainerSize() {
+    const container = document.querySelector(".container");
+    container.style.height = `${window.innerHeight}px`;
 }
 
-grid.style.width = `${gridItem.offsetWidth * gridWidth}px`;
-container.style.height = `${window.innerHeight}px`;
+function drawGrid() {
+    const grid = document.querySelector(".grid");
+
+    const gridWidth = 16;
+    let gridItem;
+    for (let i = 0; i < gridWidth * gridWidth; i++) {
+        gridItem = document.createElement("div");
+        gridItem.classList.toggle("grid-item");
+        grid.appendChild(gridItem);
+    }
+
+    grid.style.width = `${gridItem.offsetWidth * gridWidth}px`;
+}
